85e42bf9dd80eb1518c8212b0e6114f2a5476ef3
Vickyciou <stu7032@gmail.com>
2024-06-18 17:38:51 +0800

    Initial Commit

---
+++ BJ4Practices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BJ4Practices
+//
+//  Created by Vicky on 2024/6/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
